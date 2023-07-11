f94ba108edd3cf021f2ce02d77f15afe3133be2b
Milind Mishra <milind.mishra4@gmail.com>
2023-07-12 01:38:11 +0800

    📦 NEW: hash loading spinner in meme page

--- components/Memes.js
+++ components/Memes.js
@@ -1,16 +1,39 @@
 import { useEffect, useState } from "react"
+import { HashLoader } from "react-spinners"
 
 const Memes = () => {
+  const [spinner, setSpinner] = useState(false)
   const [currentMeme, setCurrentMeme] = useState(null)
-  useEffect(() => {
+
+  function fetchMeme() {
+    setSpinner(true)
     fetch("https://meme-api.com/gimme")
       .then((res) => res.json())
       .then((data) => {
         setCurrentMeme(data)
+        setSpinner(false)
       })
       .catch((err) => console.log(err))
+  }
+
+  useEffect(() => {
+    fetchMeme()
   }, [])
 
+  if (spinner) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "50vh",
+        }}
+      >
+        <HashLoader color="#39D353" />
+      </div>
+    )
+  }
   return (
     <>
       <a
