2a921da26b9cc53969a82ce8825df25d43e2cc36
Milind Mishra <milind.mishra4@gmail.com>
2023-07-11 20:28:38 +0800

    📦 NEW: added memes

---
+++ components/Memes.js
@@ -0,0 +1,29 @@
+import Image from "next/image"
+import { useEffect, useState } from "react"
+
+const Memes = () => {
+  const [currentMeme, setCurrentMeme] = useState(null)
+  useEffect(() => {
+    fetch("https://meme-api.com/gimme")
+      .then((res) => res.json())
+      .then((data) => {
+        setCurrentMeme(data)
+      })
+      .catch((err) => console.log(err))
+  }, [])
+
+  return (
+    <>
+      <a
+        title="Enjoy Memes"
+        className="figure"
+        href={currentMeme ? currentMeme.postLink : "#"}
+        target="_blank"
+      >
+        {currentMeme && <img src={currentMeme.url} alt="Enjoy Memes" />}
+      </a>
+    </>
+  )
+}
+
+export default Memes
