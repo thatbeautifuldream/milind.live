3f7c728d8404b1aa43da31a2ba15b6d7b8e99213
Milind Mishra <milind@Milinds-M1-Air.local>
2023-07-25 20:38:39 +0800

    📦 NEW: leetcode tooltip

--- components/LeetcodeStats.js
+++ components/LeetcodeStats.js
@@ -1,13 +1,28 @@
 import React, { useState, useEffect } from "react"
 import axios from "axios"
+import { Tooltip } from "react-tooltip"
+import "react-tooltip/dist/react-tooltip.css"
 
 function LeetcodeStats({ username }) {
   const [data, setData] = useState(null)
+
+  const LeetcodeTooltip = ({ message, children }) => {
+    return (
+      <a
+        data-tooltip-id="my-tooltip"
+        data-tooltip-content={message || "Leetcode Stats"}
+        style={{ cursor: "pointer" }}
+      >
+        {children}
+      </a>
+    )
+  }
 
   useEffect(() => {
     axios
       .get(`https://leetcode-stats-api.herokuapp.com/${username}`)
       .then((result) => {
+        console.log("Leetcode Stats : ", result.data)
         setData(result.data)
       })
       .catch((error) => {
@@ -19,47 +34,64 @@ function LeetcodeStats({ username }) {
 
   return (
     <div>
+      <Tooltip id="my-tooltip" />
       <table>
         <tbody>
           <tr>
             <td>Total Solved / Total Questions</td>
-            <td>
-              {data.totalSolved} / {data.totalQuestions}
-            </td>
+            <LeetcodeTooltip>
+              <td>
+                {data.totalSolved} / {data.totalQuestions}
+              </td>
+            </LeetcodeTooltip>
           </tr>
           <tr>
             <td>Easy Solved / Total Easy</td>
-            <td>
-              {data.easySolved} / {data.totalEasy}
-            </td>
+            <LeetcodeTooltip>
+              <td>
+                {data.easySolved} / {data.totalEasy}
+              </td>
+            </LeetcodeTooltip>
           </tr>
           <tr>
             <td>Medium Solved / Total Medium</td>
-            <td>
-              {data.mediumSolved} / {data.totalMedium}
-            </td>
+            <LeetcodeTooltip>
+              <td>
+                {data.mediumSolved} / {data.totalMedium}
+              </td>
+            </LeetcodeTooltip>
           </tr>
           <tr>
             <td>Hard Solved / Total Hard</td>
-            <td>
-              {data.hardSolved} / {data.totalHard}
-            </td>
+            <LeetcodeTooltip>
+              <td>
+                {data.hardSolved} / {data.totalHard}
+              </td>
+            </LeetcodeTooltip>
           </tr>
           <tr>
             <td>Acceptance Rate</td>
-            <td>{data.acceptanceRate}</td>
+            <LeetcodeTooltip>
+              <td>{data.acceptanceRate}</td>
+            </LeetcodeTooltip>
           </tr>
           <tr>
             <td>Ranking</td>
-            <td>{data.ranking}</td>
+            <LeetcodeTooltip>
+              <td>{data.ranking}</td>
+            </LeetcodeTooltip>
           </tr>
           <tr>
             <td>Contribution Points</td>
-            <td>{data.contributionPoints}</td>
+            <LeetcodeTooltip>
+              <td>{data.contributionPoints}</td>
+            </LeetcodeTooltip>
           </tr>
           <tr>
             <td>Reputation</td>
-            <td>{data.reputation}</td>
+            <LeetcodeTooltip>
+              <td>{data.reputation}</td>
+            </LeetcodeTooltip>
           </tr>
         </tbody>
       </table>
