dd9dc30f70621e6d4e01d56c5f9edf4b18b5502a
Milind Mishra ▲ <milind.mishra4@gmail.com>
2023-09-14 22:34:24 +0530

    🐛 FIX: lets keep it dark

--- components/Sign.js
+++ components/Sign.js
@@ -1,31 +1,22 @@
-import React, { useEffect, useState } from "react"
 import Image from "next/image"
 import milindsSignDark from "../public/images/sign/milind-sign-dark.svg"
-import milindsSignLight from "../public/images/sign/milind-sign-light.svg"
+// import milindsSignLight from "../public/images/sign/milind-sign-light.svg"
+// import { useDarkMode } from "usehooks-ts"
 
 export default function Sign() {
-  const [isDarkMode, setIsDarkMode] = useState(false)
-
-  useEffect(() => {
-    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)")
-    setIsDarkMode(mediaQuery.matches)
-    const handleChange = (e) => {
-      setIsDarkMode(e.matches)
-    }
-    mediaQuery.addEventListener("change", handleChange)
-    return () => {
-      mediaQuery.removeEventListener("change", handleChange)
-    }
-  }, [])
-
+  //   const { isDarkMode } = useDarkMode()
+  //   console.log("isDarkMode : ", isDarkMode)
   return (
     <>
-      <a title="Milind's Sign" className="sign">
+      {/* <a title="Milind's Sign" className="sign">
         {isDarkMode ? (
           <Image src={milindsSignDark} alt="Dark Sign" />
         ) : (
           <Image src={milindsSignLight} alt="Light Sign" />
         )}
+      </a> */}
+      <a title="Milind's Sign" className="sign">
+        <Image src={milindsSignDark} alt="Dark Sign" />
       </a>
     </>
   )
