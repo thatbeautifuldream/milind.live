49de11fed4d1aea4fa12be02cc2316b76a5a9137
Milind Mishra ▲ <milind.mishra4@gmail.com>
2023-09-20 17:29:04 +0530

    📦 NEW: next link

--- components/Verified.js
+++ components/Verified.js
@@ -1,12 +1,19 @@
 import Image from "next/image"
 import MishraVerified from "../public/images/stamp/verified-stamp.png"
+import Link from "next/link"
 
 export default function Verified() {
   return (
     <>
-      <a href="/" title="Mishra Verified" className="footer-verified-stamp">
-        <Image src={MishraVerified} alt="Mishra Verified" placeholder="blur" />
-      </a>
+      <Link href="/">
+        <a title="Mishra Verified" className="footer-verified-stamp">
+          <Image
+            src={MishraVerified}
+            alt="Mishra Verified"
+            placeholder="blur"
+          />
+        </a>
+      </Link>
     </>
   )
 }
