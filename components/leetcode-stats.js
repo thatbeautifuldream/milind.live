d559e90edda2dcbc7f8f3a65272a011c4eba035e
Milind Mishra ▲ <milind.mishra4@gmail.com>
2023-09-28 10:36:09 +0530

    📦 NEW: cobe added

--- components/leetcode-stats.js
+++ components/leetcode-stats.js
@@ -1,7 +1,6 @@
 import axios from "axios"
 import React, { useEffect, useState } from "react"
 import BarLoader from "react-spinners/BarLoader"
-import { DonutChart } from "@tremor/react"
 
 const cities = [
   {
