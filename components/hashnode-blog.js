a56fa706b8031a7090138165612e704fc81cd4c0
Milind Mishra ▲ <milind.mishra4@gmail.com>
2023-11-01 09:12:15 +0530

    📦 NEW: move to react query

--- components/hashnode-blog.js
+++ components/hashnode-blog.js
@@ -1,90 +1,89 @@
-import React, { useState, useEffect } from "react"
+import React from "react"
+import { useQuery } from "@tanstack/react-query"
 import Link from "next/link"
 import BarLoader from "react-spinners/BarLoader"
 
-const BlogPost = ({ slug }) => {
-  const [post, setPost] = useState(null)
-  const [isLoading, setIsLoading] = useState(true)
-  const [error, setError] = useState(null)
-
-  useEffect(() => {
-    const fetchPost = async () => {
-      try {
-        const query = `
-          query GetPost($slug: String!) {
-            publication(host: "blog.milind.live") {
-              post(slug: $slug) {
-                title
-                slug
-                content {
-                  html
-                }
-                coverImage {
-                  url
-                }
-              }
-            }
+const fetchPost = async (slug) => {
+  const query = `
+    query GetPost($slug: String!) {
+      publication(host: "blog.milind.live") {
+        post(slug: $slug) {
+          title
+          slug
+          content {
+            html
           }
-        `
-
-        const variables = {
-          slug,
+          coverImage {
+            url
+          }
         }
-
-        const response = await fetch("https://gql.hashnode.com", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            query,
-            variables,
-          }),
-        })
-
-        const data = await response.json()
-        console.log({ data })
-        if (data.errors && data.errors.length > 0) {
-          throw new Error(data.errors[0].message)
-        }
-
-        setPost(data.data.publication.post)
-        setIsLoading(false)
-      } catch (error) {
-        console.error("Error fetching post data:", error.message)
-        setError(error.message)
-        setIsLoading(false)
       }
     }
+  `
 
-    fetchPost()
-  }, [])
+  const variables = {
+    slug,
+  }
+
+  const response = await fetch("https://gql.hashnode.com", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  })
+
+  const data = await response.json()
+  if (data.errors && data.errors.length > 0) {
+    throw new Error(data.errors[0].message)
+  }
+
+  return data.data.publication.post
+}
+
+const BlogPost = ({ slug }) => {
+  const {
+    data: post,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["blogPost"],
+    queryFn: () => fetchPost(slug),
+  })
+
+  if (isLoading) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "1.25rem",
+        }}
+      >
+        <BarLoader color="#006D32" />
+      </div>
+    )
+  }
+
+  if (isError) {
+    return <div>Error fetching post data</div>
+  }
 
   return (
     <div>
-      {isLoading ? (
-        <BarLoader
-          color="#006D32"
-          cssOverride={{
-            display: "flex",
-            justifyContent: "center",
-            margin: "auto",
-          }}
-        />
-      ) : (
-        <>
-          <Link href="/blogs">← Back to blogs</Link>
-          <img
-            src={post?.coverImage.url}
-            alt={post?.title}
-            style={{
-              borderRadius: "0.5rem",
-            }}
-          />
-          <h1>{post?.title}</h1>
-          <div dangerouslySetInnerHTML={{ __html: post?.content.html }} />
-        </>
-      )}
+      <Link href="/blogs">← Back to blogs</Link>
+      <img
+        src={post?.coverImage.url}
+        alt={post?.title}
+        style={{
+          borderRadius: "0.5rem",
+        }}
+      />
+      <h1>{post?.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: post?.content.html }} />
     </div>
   )
 }
--- components/hashnode-blogs.js
+++ components/hashnode-blogs.js
@@ -1,7 +1,46 @@
-import React, { useState, useEffect } from "react"
+import React from "react"
+import { useQuery } from "@tanstack/react-query"
 import BarLoader from "react-spinners/BarLoader"
 
-export function Insite({ title, coverImage, url, description }) {
+const fetchBlogPosts = async () => {
+  const query = `
+    query {
+      publication(host: "blog.milind.live") {
+        posts(first: 10) {
+          edges {
+            node {
+              title
+              slug
+              brief 
+              url
+              coverImage {
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+  `
+
+  const variables = {}
+
+  const response = await fetch("https://gql.hashnode.com", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  })
+
+  const data = await response.json()
+  return data.data.publication.posts.edges
+}
+
+function Insite({ title, coverImage, url, description }) {
   const H3 = "h3"
   return (
     <a className="insite-card block font-semibold" href={url}>
@@ -19,57 +58,32 @@ export function Insite({ title, coverImage, url, description }) {
 }
 
 const BlogPosts = () => {
-  const [posts, setPosts] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
+  const {
+    data: posts,
+    isPending,
+    error,
+  } = useQuery({
+    queryKey: ["blogPosts"],
+    queryFn: fetchBlogPosts,
+  })
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const query = `
-        query {
-          publication(host: "blog.milind.live") {
-            posts(first: 10) {
-              edges {
-                node {
-                  title
-                  slug
-                  brief 
-                  url
-                  coverImage {
-                    url
-                  }
-                }
-              }
-            }
-          }
-        }
-      `
+  if (isPending) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "1.25rem",
+        }}
+      >
+        <BarLoader color="#006D32" />
+      </div>
+    )
+  }
 
-      const variables = {}
-
-      try {
-        const response = await fetch("https://gql.hashnode.com", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            query,
-            variables,
-          }),
-        })
-
-        const data = await response.json()
-        console.log({ data })
-        setPosts(data.data.publication.posts.edges)
-        setIsLoading(false) // Set loading to false after data is fetched
-      } catch (error) {
-        console.error("Error fetching data:", error)
-        setIsLoading(false) // Set loading to false in case of error
-      }
-    }
-
-    fetchData()
-  }, [])
+  if (error) {
+    return <div>Error fetching data</div>
+  }
 
   return (
     <div
@@ -80,26 +94,15 @@ const BlogPosts = () => {
         marginTop: "1.25rem",
       }}
     >
-      {isLoading ? (
-        <BarLoader
-          color="#006D32"
-          cssOverride={{
-            display: "flex",
-            justifyContent: "center",
-            margin: "auto",
-          }}
+      {posts.map((post, index) => (
+        <Insite
+          title={post.node.title}
+          coverImage={post.node.coverImage.url}
+          url={`/blogs/${post.node.slug}`}
+          description={post.node.brief}
+          key={index}
         />
-      ) : (
-        posts.map((post, index) => (
-          <Insite
-            title={post.node.title}
-            coverImage={post.node.coverImage.url}
-            url={`/blogs/${post.node.slug}`}
-            description={post.node.brief}
-            key={index}
-          />
-        ))
-      )}
+      ))}
     </div>
   )
 }
