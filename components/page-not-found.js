8f7c6b1a33036d1dca10bba3d56bbe6ea02a5c30
Milind Mishra <milind.mishra4@gmail.com>
2024-01-26 01:46:44 +0530

    [NEW] changes

--- components/page-not-found.js
+++ components/page-not-found.js
@@ -1,12 +1,10 @@
-import Error from "next/error"
 import YouTube from "react-youtube"
 
 export default function PageNotFound() {
-  const videoId = "cKSpFkMr6Ik"
+  const videoId = "dQw4w9WgXcQ"
   return (
     <>
       <YouTube videoId={videoId} />
-      <Error statusCode={404} />
     </>
   )
 }
