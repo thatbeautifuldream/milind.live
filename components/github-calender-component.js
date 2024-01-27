43f1da2c8c456c0c7bda22605415d7d33801bbb7
Milind Mishra <milind.mishra4@gmail.com>
2024-01-27 13:32:38 +0530

    [FIX] add github link

--- components/github-calender-component.js
+++ components/github-calender-component.js
@@ -1,8 +1,13 @@
 import React from "react"
 import { useTheme } from "next-themes"
 import GitHubCalendar from "react-github-calendar"
+import Link from "next/link"
 
 export default function GithubCalenderComponent({ username }) {
   const { theme } = useTheme()
-  return <GitHubCalendar username={username} blockSize={10} />
+  return (
+    <Link href="https://github.com/thatbeautifuldream" target="_blank">
+      <GitHubCalendar username={username} blockSize={10} />
+    </Link>
+  )
 }
