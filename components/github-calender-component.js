99a8bac39bf161dfc6736d2f03fe2e045f9953bb
Milind Mishra <milind.mishra4@gmail.com>
2024-01-27 13:29:29 +0530

    [NEW] update react cal

--- components/github-calender-component.js
+++ components/github-calender-component.js
@@ -1,31 +1,8 @@
 import React from "react"
 import { useTheme } from "next-themes"
 import GitHubCalendar from "react-github-calendar"
-import { Tooltip as ReactTooltip } from "react-tooltip"
-import "react-tooltip/dist/react-tooltip.css"
 
 export default function GithubCalenderComponent({ username }) {
-  const { resolvedTheme } = useTheme()
-  const theme = resolvedTheme === "dark" ? "dark" : "light"
-  return (
-    <>
-      <GitHubCalendar
-        username={username}
-        hideColorLegend="true"
-        showWeekdayLabels="true"
-        style={{
-          padding: "0px",
-          marginBottom: "20px",
-        }}
-        colorScheme={theme}
-        renderBlock={(block, activity) =>
-          React.cloneElement(block, {
-            "data-tooltip-id": "react-tooltip",
-            "data-tooltip-html": `${activity.count} commits on ${activity.date}`,
-          })
-        }
-      />
-      <ReactTooltip id="react-tooltip" />
-    </>
-  )
+  const { theme } = useTheme()
+  return <GitHubCalendar username={username} blockSize={10} />
 }
--- components/memes.js
+++ components/memes.js
@@ -1,18 +1,10 @@
-import { useMutation, useQuery } from "@tanstack/react-query"
 import axios from "axios"
 import React from "react"
+import { useQuery } from "@tanstack/react-query"
 import { BarLoader } from "react-spinners"
 
 async function fetchMeme() {
   const response = await axios.get("https://meme-api.com/gimme")
-  return response.data
-}
-
-async function postImage(imageUrl) {
-  console.log("imageUrl", imageUrl)
-  const response = await axios.post("/api/bot", {
-    imageUrl,
-  })
   return response.data
 }
 
@@ -25,18 +17,6 @@ export default function Memes() {
   } = useQuery({
     queryKey: ["meme"],
     queryFn: fetchMeme,
-  })
-
-  // mutation to post imageUrl to a /api/bot endpoint
-  const {
-    data,
-    isError: isBotError,
-    isLoading: isBotLoading,
-    isSuccess,
-    mutateAsync,
-  } = useMutation({
-    mutationKey: ["bot"],
-    mutationFn: postImage,
   })
 
   return (
@@ -93,37 +73,6 @@ export default function Memes() {
               }}
             />
           </div>
-          <code
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              margin: "1rem 0",
-            }}
-          >
-            <button
-              onClick={() => {
-                console.log("clicked")
-                mutateAsync(currentMeme.url)
-              }}
-            >
-              {!isBotLoading && "Describe this image using AI"}
-              {isBotLoading && "AI Magic"}
-            </button>
-          </code>
-          <code>
-            {isBotLoading && <BarLoader color="#006D32" />}
-            {isBotError && (
-              <code
-                style={{
-                  color: "#ff0000",
-                  display: "flex",
-                }}
-              >
-                Error!
-              </code>
-            )}
-            {isSuccess && <code>{data?.kwargs?.content}</code>}
-          </code>
         </>
       )}
     </div>
