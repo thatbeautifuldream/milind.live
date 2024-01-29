5d5e6b78bdbdbffb557bad01c0e429712585e4ce
Milind Mishra <milind.mishra4@gmail.com>
2024-01-29 09:10:50 +0530

    [FIX] light and dark mode pill badges

--- components/badge.js
+++ components/badge.js
@@ -1,7 +1,9 @@
 export default function Badge({ text }) {
   return (
-    <span className="inline-flex items-center rounded-md bg-green-400/10 px-2 py-1 text-xs font-medium text-green-400 ring-1 ring-inset ring-green-400/20">
-      {text}
-    </span>
+    <>
+      <span className="inline-flex items-center rounded-md bg-green-50 dark:bg-green-400/10 px-2 py-1 text-xs font-medium text-green-800 dark:text-green-400 ring-1 ring-inset ring-green-600/20 dark:ring-green-400/20">
+        {text}
+      </span>
+    </>
   )
 }
