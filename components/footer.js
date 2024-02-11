4f8b3d9f899e873dbf3642ded8c82bdfc586dd2f
Milind Mishra <milind.mishra4@gmail.com>
2024-02-12 01:53:16 +0530

    [FIX] footer text update

--- components/footer.js
+++ components/footer.js
@@ -107,8 +107,8 @@ export default function Footer() {
           ))}
         </div>
         <p className="mt-10 text-center text-xs leading-5 text-gray-500">
-          &copy; {new Date().getFullYear()} Milind Mishra, Inc. All rights
-          securely tucked away.
+          &copy; {new Date().getFullYear()} Milind Mishra, Inc. All rights. No
+          wrongs. Deal with it.
         </p>
       </div>
     </footer>
