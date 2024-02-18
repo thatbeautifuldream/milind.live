71a164d176a7b26f54f7a11d87141587157482db
Milind Mishra <milind.mishra4@gmail.com>
2024-02-18 20:36:49 +0530

    [NEW] update badge with caution text and manage alert

--- components/github-calender-component.js
+++ components/github-calender-component.js
@@ -1,4 +1,3 @@
-import Link from "next/link"
 import { useTheme } from "nextra-theme-blog"
 import React from "react"
 import GitHubCalendar from "react-github-calendar"
@@ -6,12 +5,14 @@ import GitHubCalendar from "react-github-calendar"
 export default function GithubCalenderComponent({ username }) {
   const { theme } = useTheme()
   return (
-    <Link href="https://github.com/thatbeautifuldream" target="_blank">
-      <GitHubCalendar
-        username={username}
-        blockSize={10}
-        colorScheme={theme === "dark" ? "dark" : "light"}
-      />
-    </Link>
+    <GitHubCalendar
+      username={username}
+      blockSize={10}
+      colorScheme={theme === "dark" ? "dark" : "light"}
+      style={{
+        paddingTop: "2rem",
+        paddingBottom: "2rem",
+      }}
+    />
   )
 }
